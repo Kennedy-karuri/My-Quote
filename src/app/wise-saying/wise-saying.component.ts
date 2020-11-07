0c2c78ae9058d3fb6356ec14df9c8bcb2f548e4b
Kennedy-karuri <kennkaruri99@gmail.com>
2020-11-07 12:02:09 +0300

    created  wise saying component

---
+++ src/app/wise-saying/wise-saying.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wise-saying',
+  templateUrl: './wise-saying.component.html',
+  styleUrls: ['./wise-saying.component.css']
+})
+export class WiseSayingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
