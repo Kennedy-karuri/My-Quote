1382408cfc4776e8a2fd3ebd2c94ccfa86487779
Kennedy-karuri <kennkaruri99@gmail.com>
2020-11-08 11:34:31 +0300

    modified app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WiseSayingComponent } from './wise-saying/wise-saying.component';
 import { QuoteComponent } from './quote/quote.component';
 import { FormsComponent } from './forms/forms.component';
+import { from } from 'rxjs';
 
 
 @NgModule({
@@ -18,7 +20,8 @@ import { FormsComponent } from './forms/forms.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
