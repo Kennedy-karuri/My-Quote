0c04513baab64bf559401f68278f65b4a50f46c5
Kennedy-karuri <kennkaruri99@gmail.com>
2020-11-08 13:07:02 +0300

    modified quotes component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [ 
-    new Quote(1, 'MARTIN lUTHER', 'yolo','Steve',new Date(2020,4,15)),
-    new Quote(2, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', 'hey you',new Date()),
-    new Quote(3, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', 'hey you',new Date()),
-    new Quote(4, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', 'hey you',new Date()),
-    new Quote(5, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', 'hey you',new Date()),
+    new Quote(1, 'MARTIN lUTHER', 'I have a dream','Steve',new Date(2020,4,15)),
+    new Quote(2, 'SOCRATES', 'The unexamined life is not worth living.', 'Petrah',new Date(2020,7,30)),
+    new Quote(3, 'SOCRATES', 'Wonder is the beginning of wisdom', 'Ken',new Date(2019,6,27)),
+    new Quote(4, 'Aristotle', 'The roots of education … are bitter, but the fruit is sweet.', 'Farzana',new Date(2019,8,29)),
+    new Quote(5, 'Sir Isaac Newton', 'If I have seen further it is by standing on the shoulders of giants', 'Carol',new Date(2020,8,6)),
   ];  
   constructor() { }
   
