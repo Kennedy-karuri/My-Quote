44914e0dd4c9aa8c51f3f8507a60ad07a4009973
Kennedy-karuri <kennkaruri99@gmail.com>
2020-11-08 11:33:40 +0300

    created class quote

---
+++ src/app/quote.ts
@@ -0,0 +1,18 @@
+export class Quote {
+    id: number;
+    author : string;
+    description : string;
+    user: string;
+    day:Date;
+    upvote: number;
+    downvote: number;
+    constructor(id: number, author: string, description: string, user: string, day: Date) {
+        this.id = id;
+        this.author = author;
+        this.description = description;
+        this.user = user;
+        this.day = day;
+
+    }
+}
+
