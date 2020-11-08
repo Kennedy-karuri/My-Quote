98a7d314ab05b95ce41458c0791f5660372444b6
Kennedy-karuri <kennkaruri99@gmail.com>
2020-11-08 11:35:38 +0300

    modified quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  quotes: Quote[] = [ 
+    new Quote(1, 'MARTIN lUTHER', 'yolo','Steve',new Date(2020,4,15)),
+    new Quote(2, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', 'hey you',new Date()),
+    new Quote(3, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', 'hey you',new Date()),
+    new Quote(4, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', 'hey you',new Date()),
+    new Quote(5, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', 'hey you',new Date()),
+  ];  
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
